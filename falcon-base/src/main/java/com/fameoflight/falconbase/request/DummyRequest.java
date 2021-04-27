3957263cd0162c55db4c1eaf75cbd2299e52bf47
Md. Danish <mohd.ansari@freecharge.com>
2021-04-28 01:00:38 +0530

    basic code

---
+++ falcon-base/src/main/java/com/fameoflight/falconbase/request/DummyRequest.java
@@ -0,0 +1,13 @@
+package com.fameoflight.falconbase.request;
+
+public class DummyRequest {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
